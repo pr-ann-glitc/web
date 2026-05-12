29fc0ed3a1271396aed0d1b3f3b3ea5f33d43157
Anna <prokudinaanna2006.ap@gmail.com>
2026-05-12 23:50:08 +0700

    nn

--- lab_js/8/script.js
+++ lab_js/8/script.js
@@ -1,115 +1,115 @@
-// // Написать функцию counter(n), которая выводит в консоль раз в секунду числа n, n-1 ... 2, 1, 0 и останавливается
-// function counter(n) {
-//     let counter = n;
-//     const timerId = setInterval(() => { 
-//         console.log(counter);
-//         document.getElementById('result1').textContent = counter;
-//         if (counter === 0) {
-//             clearInterval(timerId);
-//         } 
-//         counter--;
-//     }, 1000);
-// }
-// document.getElementsByClassName('button-start')[0].addEventListener('click', function() {
-//     const val = document.getElementById('input-task1').value;
-//     counter(val);
-// });
+// Написать функцию counter(n), которая выводит в консоль раз в секунду числа n, n-1 ... 2, 1, 0 и останавливается
+function counter(n) {
+    let counter = n;
+    const timerId = setInterval(() => { 
+        console.log(counter);
+        document.getElementById('result1').textContent = counter;
+        if (counter === 0) {
+            clearInterval(timerId);
+        } 
+        counter--;
+    }, 1000);
+}
+document.getElementsByClassName('button-start')[0].addEventListener('click', function() {
+    const val = document.getElementById('input-task1').value;
+    counter(val);
+});
 
 
 
 
-// //Написать функцию createCounter(n), возвращающую объект с методами:
-// // start() -- запускает (или возобновляет) счётчик c интервалом 1 секунда: N, N-1.
-// // pause() -- приостанавливает счёт, но не сбрасывает счётчик.
-// // stop() -- останавливает счёт, сбрасывает счётчик.
-// function createCounter(n) {
-//     let count = n;
-//     let timerId = null;
-//     const timer = {
-//         start() {
-//             if (timerId !== null) {
-//                 clearInterval(timerId);
-//                 timerId = null;
-//             }
-//             timerId = setInterval(() => {
-//                 document.getElementById('result2').textContent = count;
-//                 if (count >= 0) {
-//                     count--;
-//                 } else {
-//                     this.stop()
-//                 }
-//             }, 1000);
-//         },
-//         pause() {
-//             if (timerId !== null) {
-//                 clearInterval(timerId);
-//                 timerId = null;
-//             }
-//         },
-//         stop() {
-//             clearInterval(timerId);
-//             timerId = null;
-//             count = n;
-//             document.getElementById('result2').textContent = null;
-//         }
-//     }
-//     return timer;
-// }
+//Написать функцию createCounter(n), возвращающую объект с методами:
+// start() -- запускает (или возобновляет) счётчик c интервалом 1 секунда: N, N-1.
+// pause() -- приостанавливает счёт, но не сбрасывает счётчик.
+// stop() -- останавливает счёт, сбрасывает счётчик.
+function createCounter(n) {
+    let count = n;
+    let timerId = null;
+    const timer = {
+        start() {
+            if (timerId !== null) {
+                clearInterval(timerId);
+                timerId = null;
+            }
+            timerId = setInterval(() => {
+                document.getElementById('result2').textContent = count;
+                if (count >= 0) {
+                    count--;
+                } else {
+                    this.stop()
+                }
+            }, 1000);
+        },
+        pause() {
+            if (timerId !== null) {
+                clearInterval(timerId);
+                timerId = null;
+            }
+        },
+        stop() {
+            clearInterval(timerId);
+            timerId = null;
+            count = n;
+            document.getElementById('result2').textContent = null;
+        }
+    }
+    return timer;
+}
 
-// let currentCounter = null;
-// document.getElementById('input-task2').addEventListener('blur', function() {
-//     const val = document.getElementById('input-task2').value;
-//     currentCounter = createCounter(val);
-// });
+let currentCounter = null;
+document.getElementById('input-task2').addEventListener('blur', function() {
+    const val = document.getElementById('input-task2').value;
+    currentCounter = createCounter(val);
+});
 
-// document.getElementById('button-start').addEventListener('click', function() {
-//     currentCounter.start();
-// });
+document.getElementById('button-start').addEventListener('click', function() {
+    currentCounter.start();
+});
 
-// document.getElementById('button-pause').addEventListener('click', function() {
-//     if (currentCounter) {
-//         currentCounter.pause();
-//         document.getElementById('result2').textContent += " pause";
-//     }
-// });
+document.getElementById('button-pause').addEventListener('click', function() {
+    if (currentCounter) {
+        currentCounter.pause();
+        document.getElementById('result2').textContent += " pause";
+    }
+});
 
-// document.getElementById('button-stop').addEventListener('click', function() {
-//     if (currentCounter) currentCounter.stop();
-// });
-
-
-
-
-// // Написать функцию delay(N), возвращающую промис, который сделает resolve() через N секунд.
-// function delay(n) {
-//     const time = n*1000;
-//     const userPromise = new Promise((resolve) => {
-//         setTimeout(() => {resolve()}, time)
-//     })
-//     return userPromise;
-// }
-
-// document.getElementById('button-resolve').addEventListener('click', function() {
-//     const userN = document.getElementById('input-task3').value
-//     delay(userN).then(
-//         result => document.getElementById('result3').textContent = "time is over"
-//     );
-//     document.getElementById('result3').textContent = null;
-// })
+document.getElementById('button-stop').addEventListener('click', function() {
+    if (currentCounter) currentCounter.stop();
+});
 
 
 
-// // Решить задачу со счётчиком N, N-1 ... 2, 1, 0 через функцию delay.
-// function newCounter(n) {
-//     let count = n;
-//     document.getElementById('result4').textContent = count;
-//     if (count === 0) return;
-//     delay(1).then(() => newCounter(n-1))
-// }
-// document.getElementById('button-resolve1').addEventListener('click', function() {
-//     const userN = document.getElementById('input-task4').value
-//     newCounter(userN)
-// })
+
+// Написать функцию delay(N), возвращающую промис, который сделает resolve() через N секунд.
+function delay(n) {
+    const time = n*1000;
+    const userPromise = new Promise((resolve) => {
+        setTimeout(() => {resolve()}, time)
+    })
+    return userPromise;
+}
+
+document.getElementById('button-resolve').addEventListener('click', function() {
+    const userN = document.getElementById('input-task3').value
+    delay(userN).then(
+        result => document.getElementById('result3').textContent = "time is over"
+    );
+    document.getElementById('result3').textContent = null;
+})
+
+
+
+// Решить задачу со счётчиком N, N-1 ... 2, 1, 0 через функцию delay.
+function newCounter(n) {
+    let count = n;
+    document.getElementById('result4').textContent = count;
+    if (count === 0) return;
+    delay(1).then(() => newCounter(n-1))
+}
+document.getElementById('button-resolve1').addEventListener('click', function() {
+    const userN = document.getElementById('input-task4').value
+    newCounter(userN)
+})
 
 
 // Написать функцию, возвращающую название первого репозитория на github.com по имени пользователя 
